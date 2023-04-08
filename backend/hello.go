da416c95331d69739f27d52a9cda00a404cc81fe
adenysenko <a.denysenko@vehikl.com>
2023-04-08 10:08:05 -0400

    Create a simple endpoint

--- backend/hello.go
+++ backend/hello.go
@@ -1,8 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
 
-func main() {
-    fmt.Println("Hello, World!")
+	"github.com/gin-gonic/gin"
+)
+
+type cocktail struct {
+	ID       int    `json:"id"`
+	Title    string `json:"title"`
+	ImageURL string `json:"image_url"`
 }
 
+type ingredient struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+type cocktail_ingredient struct {
+	CocktailID   int `json:"cocktail_id"`
+	IngredientID int `json:"ingredient_id"`
+	Quantity     int `json:"quantity"`
+}
+
+var cocktails = []cocktail{
+	{ID: 1, Title: "Mojito", ImageURL: "https://www.thecocktaildb.com/images/media/drink/3z6xdi1589574608.jpg"},
+	{ID: 2, Title: "Margarita", ImageURL: "https://www.thecocktaildb.com/images/media/drink/5noda61589575158.jpg"},
+	{ID: 3, Title: "Long Island Iced Tea", ImageURL: "https://www.thecocktaildb.com/images/media/drink/loezxn1504373874.jpg"},
+}
+
+func getCocktails(context *gin.Context) {
+	context.IndentedJSON(http.StatusOK, cocktails)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/cocktails", getCocktails)
+
+	router.Run("localhost:8080")
+}
