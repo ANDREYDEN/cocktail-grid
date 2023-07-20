e246784d16f82b6e52350186f030c95fcc0d2a6f
adenysenko <a.denysenko@vehikl.com>
2023-07-20 18:49:06 -0400

    Add style to table

--- frontend/src/components/CocktailGrid.vue
+++ frontend/src/components/CocktailGrid.vue
@@ -32,21 +32,21 @@ const getIngredients = async () => {
 </script>
 
 <template>
-  <h1>Cocktail Grid</h1>
+  <h1 class="text-center text-5xl">Cocktail Grid</h1>
 
   <div v-if="!cocktails || !ingredients">
     <p>Loading...</p>
   </div>
-  <div v-else>
+  <div v-else class="m-10">
     <div class="flex flex-row w-full">
-      <h2 class="flex-1"></h2>
-      <h2 v-for="ingredient in ingredients" :key="ingredient.id" class="flex-1">
+      <h2 class="flex-1 border"></h2>
+      <h2 v-for="ingredient in ingredients" :key="ingredient.id" class="flex-1 text-lg border">
         {{ ingredient.name }}
       </h2>
     </div>
     <div v-for="cocktail in cocktails" :key="cocktail.id" class="flex flex-row w-full justify-between">
-      <h2 class="flex-1">{{ cocktail.title }}</h2>
-      <h2 v-for="ingredient in ingredients" :key="ingredient.id" class="flex-1">
+      <h2 class="flex-1 text-lg border">{{ cocktail.title }}</h2>
+      <h2 v-for="ingredient in ingredients" :key="ingredient.id" class="flex-1 border">
         {{ cocktail.ingredients.find(ci => ci.ingredientId === ingredient.id)?.quantity ?? "" }}
       </h2>
     </div>
