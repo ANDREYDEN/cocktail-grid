5ca839cafb5ea1468832eb21685b3dc7e7eef4e0
adenysenko <a.denysenko@vehikl.com>
2024-03-27 23:51:44 -0400

    Refactor identity scopes

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -5,13 +5,21 @@ import { createAuth0 } from '@auth0/auth0-vue'
 import App from './App.vue'
 import { VueQueryPlugin } from '@tanstack/vue-query'
 
-const auth0Plugin = createAuth0({
+const identityScopes = ["profile", "email"]
+var crudScopes =  ["create:cocktail", "create:cocktail_ingredient", "delete:cocktail_ingredient", "delete:cocktail"]
+var requestedScopes = [
+    ...identityScopes,
+    ... crudScopes
+]
+var scopeString = requestedScopes.join(' ')
+
+var auth0Plugin = createAuth0({
     domain: import.meta.env.VITE_AUTH0_DOMAIN,
     clientId: import.meta.env.VITE_AUTH0_CLIENT_ID,
     authorizationParams: {
         redirect_uri: window.location.origin,
         audience: import.meta.env.VITE_AUTH0_AUDIENCE,
-        scope: "delete:cocktail_ingredient"
+        scope: scopeString
     },
 })
 
