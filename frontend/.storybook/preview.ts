0715e61181d304a4ac7ee2c650f9171147515bbb
adenysenko <a.denysenko@vehikl.com>
2024-08-15 23:26:42 -0400

    Add more stories

--- frontend/.storybook/preview.ts
+++ frontend/.storybook/preview.ts
@@ -1,3 +1,4 @@
+import '../src/styles.css'
 import type { Preview } from "@storybook/vue3";
 
 const preview: Preview = {
--- frontend/src/components/CustomButton.stories.ts
+++ frontend/src/components/CustomButton.stories.ts
@@ -4,6 +4,9 @@ import CustomButton from './CustomButton.vue';
 
 const meta: Meta<typeof CustomButton> = {
   component: CustomButton,
+  argTypes: {
+    iconPosition: { control: 'select', options: ['left', 'right']}
+  }
 };
 
 export default meta;
@@ -11,14 +14,49 @@ export default meta;
 type Story = StoryObj<typeof CustomButton>
 
 export const Outlined: Story = {
-render: (args) => ({
-    components: { CustomButton },
-    setup() {
-      return { args };
-    },
-    template: '<CustomButton v-bind="args">My Text</CustomButton>',
-  }),
+    render: (args) => ({
+        components: { CustomButton },
+        setup() {
+            return { args };
+        },
+        template: '<CustomButton v-bind="args">My Text</CustomButton>',
+    }),
     args: {
         outlined: true,
     }
 }
+
+export const Text: Story = {
+    args: {
+        outlined: false
+    },
+    render: args => ({
+        components: {
+            CustomButton
+        },
+
+        setup() {
+            return { args };
+        },
+
+        template: "<CustomButton v-bind=\"args\">My Text</CustomButton>"
+    })
+};
+
+export const Loading: Story = {
+    args: {
+        outlined: true,
+        loading: true
+    },
+    render: args => ({
+        components: {
+            CustomButton
+        },
+
+        setup() {
+            return { args };
+        },
+
+        template: "<CustomButton v-bind=\"args\">My Text</CustomButton>"
+    })
+};
