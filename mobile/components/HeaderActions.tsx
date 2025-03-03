f8e9b734f4c1db18974f84eb8466754283a6528a
adenysenko <a.denysenko@vehikl.com>
2025-03-03 14:15:37 -0500

    Create SignIn UI

--- mobile/components/HeaderActions.tsx
+++ mobile/components/HeaderActions.tsx
@@ -1,13 +1,34 @@
-import { TouchableOpacity, useColorScheme } from "react-native";
+import { useAuth } from "@/hooks/useAuth";
+import { StyleSheet, TouchableOpacity, useColorScheme, View } from "react-native";
 import MoonIcon from "./icons/MoonIcon";
+import SignInIcon from "./icons/SignInIcon";
+import SignOutIcon from "./icons/SignOutIcon";
 import SunIcon from "./icons/SunIcon";
 
 export default function HeaderActions() {
     const colorScheme = useColorScheme()
+    const { user, login } = useAuth()
 
     return (
-        <TouchableOpacity style={{ width: 24, height: 24, marginRight: 10 }}>
-            {colorScheme === 'dark' ? <MoonIcon /> : <SunIcon />}
-        </TouchableOpacity>
+        <View style={styles.container}>
+            <TouchableOpacity style={styles.iconButton} onPress={login}>
+                {!user ? <SignInIcon /> : <SignOutIcon />}
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.iconButton}>
+                {colorScheme === 'dark' ? <MoonIcon /> : <SunIcon />}
+            </TouchableOpacity>
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    iconButton: {
+        width: 24,
+        height: 24,
+        marginRight: 10,
+    }
+})
--- mobile/components/ServiceRegistrant.tsx
+++ mobile/components/ServiceRegistrant.tsx
@@ -4,7 +4,7 @@ import { Auth0Provider } from "react-native-auth0"
 export default function ServiceRegistrant({ children }: React.PropsWithChildren) {
     const queryClient = new QueryClient()
 
-    return <Auth0Provider domain={process.env.EXPO_PUBILC_AUTH0_DOMAIN} clientId={process.env.EXPO_PUBILC_AUTH0_CLIENT_ID}>
+    return <Auth0Provider domain={process.env.EXPO_PUBLIC_AUTH0_DOMAIN} clientId={process.env.EXPO_PUBLIC_AUTH0_CLIENT_ID}>
         <QueryClientProvider client={queryClient}>
             {children}
         </QueryClientProvider>
